4970dc743c16add1067d68c9457ffc9d1f4b6135
habibasaker <habiba.amr.osama@gmail.com>
2026-07-22 12:05:40 +0200

    Update task3.js

--- html/TASK5/task3.js
+++ html/TASK5/task3.js
@@ -1,8 +1,31 @@
 let wind;
 
-function openWindow(){
-    wind = window.open("child.html", "_blank", "width=350,height=350");
+function openWindow() {
+    wind = window.open(
+        "child.html",
+        "_blank",
+        "width=350,height=350,scrollbars=yes"
+    );
+
+    let scrollInterval = setInterval(() => {
+
+        if (wind.closed) {
+            clearInterval(scrollInterval);// interval of time
+            return;
+        }
+
+        wind.scrollBy(0, 10); // hynzel 10 px
+
+        if (
+            wind.innerHeight + wind.scrollY >=
+            wind.document.body.scrollHeight
+        ) {
+            wind.scrollTo(0, 0); // hyrg3 tani
+        }
+
+    }, 100);
 }
+
 
 function focusWindow(){
     if(wind){
