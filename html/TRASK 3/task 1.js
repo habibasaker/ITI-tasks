47df93884a35081a4571c22ed8bfcbfd4765e5db
habibasaker <habiba.amr.osama@gmail.com>
2026-08-08 08:57:36 +0200

    Delete html directory

--- html/TRASK 3/task 1.js
+++
@@ -1,9 +0,0 @@
-// prrompt box
-var mesg = prompt("please entre your message");
-console.log(mesg); // htb3 3ady 
-
-let i = 1
- for(;i <= 6; i++){
-    document.write("<h" + i + ">" + mesg + "</h" + i + ">")
-
-}
--- html/TRASK 3/task3.js
+++
@@ -1,6 +0,0 @@
-var num1 = Number(prompt(" 1st number:"));
-var num2 = Number(prompt(" 2nd number:"));
-
-var max = (num1 > num2) ? num1 : num2;
-
-document.write("max value = " + max);20
